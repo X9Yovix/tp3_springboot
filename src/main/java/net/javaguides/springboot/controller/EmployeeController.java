a31c8d603ad34e5ef83176e73ca79d5dd6948860
Karim Ouazzou <karimouazzou@gmail.com>
2023-10-20 21:49:47 +0100

    indicating that a class is a RESTful and making /api generic

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 
     private EmployeeService empService;
